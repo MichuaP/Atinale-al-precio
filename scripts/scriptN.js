897d5ad2ec55c0f0bc33c0885a430d93b75f1c3f
Mich <paugam21@outlook.com>
2024-03-09 20:52:31 -0600

    Comprobar que se ingresó un alias
    Se comprueba que se ingrese un alias, ya que podría quedar vacío y ocasionar errores

--- scripts/scriptN.js
+++ scripts/scriptN.js
@@ -38,37 +38,41 @@ document.addEventListener("keydown", function (event) {
 });
 
 function irJuego() {
-  localStorage.alias = document.getElementById("alias").value;
-  var links = document.getElementsByTagName("link");
-    for (var i = links.length - 1; i >= 0; i--) {
-      if (links[i].getAttribute("rel") === "stylesheet") {
-        links[i].parentNode.removeChild(links[i]);
-      }
+    const alias = document.getElementById("alias").value;
+    if (!alias) {
+        alert("Ingrese un alias");
+    } else {
+      localStorage.alias = document.getElementById("alias").value;
+      var links = document.getElementsByTagName("link");
+        for (var i = links.length - 1; i >= 0; i--) {
+          if (links[i].getAttribute("rel") === "stylesheet") {
+            links[i].parentNode.removeChild(links[i]);
+          }
+        }
+        // Carga dinámicamente el contenido de la nueva página
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'juego.html', true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            // Inserta el contenido de la nueva página en el cuerpo del documento actual
+            document.body.innerHTML = xhr.responseText;
+            audioAlias.pause();
+            // Reproduce el audio
+            audioJuego.play();
+
+            //Agregar script de juego
+            var script = document.createElement("script");
+            script.src = "scripts/juego.js"; 
+            document.body.appendChild(script);
+            
+            var scriptFont = document.createElement("script");
+            scriptFont.src = "https://kit.fontawesome.com/d60c975bf8.js";
+            scriptFont.crossOrigin = "anonymous";
+            document.body.appendChild(scriptFont);
+            
+          }
+        };
+        xhr.send();
     }
-    // Carga dinámicamente el contenido de la nueva página
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'juego.html', true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        // Inserta el contenido de la nueva página en el cuerpo del documento actual
-        document.body.innerHTML = xhr.responseText;
-        audioAlias.pause();
-        // Reproduce el audio
-        audioJuego.play();
-
-        //Agregar script de juego
-        var script = document.createElement("script");
-        script.src = "scripts/juego.js"; 
-        document.body.appendChild(script);
-        
-        var scriptFont = document.createElement("script");
-        scriptFont.src = "https://kit.fontawesome.com/d60c975bf8.js";
-        scriptFont.crossOrigin = "anonymous";
-        document.body.appendChild(scriptFont);
-        
-      }
-    };
-    xhr.send();
-
-  // window.location.href = "juego.html";
+      // window.location.href = "juego.html";
 }
